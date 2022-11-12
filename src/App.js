fbd143255add0c64ce5fb972772bfec8026d3e57
learnReactgithub <sonukushwah362@gmail.com>
2022-11-12 09:42:20 +0530

    nested component in react

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function Inner(){
+     return (
+      <h4>Innner component</h4>
+      )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h1>Nested component</h1>
+     <h4>Outer component</h4>
+     <Inner/>
+    </>
   );
 }
 
